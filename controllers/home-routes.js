c45e5086755a6965aa54dd2abb3cdc4a678e803b
ryanp <rdpodols@gmail.com>
2022-05-24 21:52:59 -0400

    started  home route get logic

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,32 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 
+//to GET all blog posts for homepage
+router.get('/', async (req, res) => {
+    try {
+        const dbPostData = await Post.findAll({
+            include: [
+                {
+                    model: Post,
+                    attributes: []
+                },
+            ],
+        });
+
+        const blogPosts = dbPostData.map((Post) => 
+        this.post.get({ plain: true })
+        );
+
+        // sends in the loggedIn session variable to the 'homepage' template
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
 
 
 module.exports = router;
