128e19657dc0cc6bdbbdb4d57f9b41cbc81fd75d
ryanp <rdpodols@gmail.com>
2022-05-27 23:34:59 -0400

    comment link on main page route added

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -48,4 +48,45 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with that ID!'});
+            return;
+        }
+        const post = dbPostData.get({ plain: true });
+
+        res.render('single-post', { post });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
